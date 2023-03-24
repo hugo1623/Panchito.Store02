23f18b1394f89bb94289c43e463275929fa58005
Hugo Luis <hugoluis.system@gmail.com>
2023-03-24 01:55:56 -0500

    Creation of the ProductRepository base in the Repository layer

---
+++ Panchito.Store02.Repository/Implementations/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Panchito.Store02.Entity;
+using Panchito.Store02.Repository.Interfaces;
+
+namespace Panchito.Store02.Repository.Implementations
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<Product> GetProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> GetProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
